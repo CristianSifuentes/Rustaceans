73399ea54fedd00e6f27dd90ba5c05a0dbe17541
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-09 11:13:21 -0600

    working with mut and let for change string

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -5,10 +5,12 @@ fn main() {
     /* This is comment */
     println!("Hello, world!");
 
-    let my_variable: &str = "This is a string";
+    let mut my_variable: &str = "This is a string";
     println!("Variable: {}" , my_variable);
     // my_variable = "change";
     // //cannot assign twice to immutable variable `my_variable`
+
+    my_variable = "Change";
     println!("{my_variable}");
     
     
