3c9f68b45fd6ad9ff6f47117ae15712c8dfcc278
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-09 11:11:53 -0600

    working with let and println!

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,15 @@
+
+// This is main point entry
 fn main() {
+    // First expression
+    /* This is comment */
     println!("Hello, world!");
+
+    let my_variable: &str = "This is a string";
+    println!("Variable: {}" , my_variable);
+    // my_variable = "change";
+    // //cannot assign twice to immutable variable `my_variable`
+    println!("{my_variable}");
     
     
 }
